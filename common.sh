fc7c231219703676bf29e8310734b60cc8913c06
xfnw <xfnw@riseup.net>
2024-06-09 15:33:51 -0400

    common: add some default rules

--- common.sh
+++ common.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source /etc/init.d/tc-functions
+
 set -eux
 
 downloadPhase() {
@@ -31,23 +33,48 @@ fixupPhase() {
 }
 
 packagePhase() {
+	tce-load -wil squashfs-tools
+	mksquashfs out "$pname.tcz" -noappend
+}
+
+metadataPhase() {
 	: doing nothing
 }
 
 submitPhase() {
-	: doing nothing
+	tce-load -wil submitqc
+	submitqc -c --libs --fix --strip "$pname.tcz"
+
+	for arg in $(cat /proc/cmdline); do
+		case "$arg" in
+			script=*)
+				SCRIPT="$(printf %s "${arg#*=}")"
+				;;
+		esac
+	done
+
+	dep= info=
+	[ -e "$pname.tcz.dep" ] && dep="-Fdep=@$pname.tcz.dep"
+	[ -e "$pname.tcz.info" ] && info="-Finfo=@$pname.tcz.info"
+
+	tce-load -wil curl
+	# XXX: dep and info intentionally unquoted
+	curl "-Ftcz=@$pname.tcz" "-Fmd5=@$pname.tcz.md5.txt" $dep $info \
+		"$SCRIPT?ver=$(getMajorVer)&arch=$(getBuild)"
 }
 
 __tinyports() {
-mkdir out
-downloadPhase
-patchPhase
-configurePhase
-buildPhase
-checkPhase
-installPhase
-fixupPhase
-packagePhase
-submitPhase
+	: "building $pname v$version"
+	mkdir out
+	downloadPhase
+	patchPhase
+	configurePhase
+	buildPhase
+	checkPhase
+	installPhase
+	fixupPhase
+	packagePhase
+	metadataPhase
+	submitPhase
 }
 
