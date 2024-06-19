83bb0035cbde07924b5daf743458d55bd1010f82
xfnw <xfnw@riseup.net>
2024-06-19 12:13:02 -0400

    buildorder: remove logic for blindly building all packages
    we need to know the tcver for future changes and you can just put a
    nonsense architecture get the same behavior of rebuilding everything

--- buildorder.py
+++ buildorder.py
@@ -16,23 +16,6 @@ def ensure_exists(pkg, parent=None):
         sys.exit(1)
 
 
-def depends(pkg, visited, parent=None):
-    if pkg in visited:
-        return
-    ensure_exists(pkg, parent=parent)
-
-    visited.add(pkg)
-    reqname = "pkgs/" + pkg + "/DEPENDS"
-    if os.path.isfile(reqname):
-        with open(reqname, "r") as reqs:
-            for dep in reqs.read().splitlines():
-                if len(dep) == 0 or dep[0] == "#":
-                    continue
-                depends(dep, visited, parent=pkg)
-
-    print(pkg)
-
-
 def rebuilds(pkg, visited, rebuilded, tcver, arch, parent=None):
     if pkg in rebuilded:
         return True
@@ -61,18 +44,18 @@ def rebuilds(pkg, visited, rebuilded, tcver, arch, parent=None):
 
 
 def main():
-    visited = set()
-
-    if len(sys.argv) > 3 and sys.argv[1] == "r":
-        tcver = sys.argv[2]
-        arch = sys.argv[3]
-        rebuilded = set()
-        for pkg in os.listdir("pkgs"):
-            rebuilds(pkg, visited, rebuilded, tcver, arch)
+    if len(sys.argv) <= 2:
+        print("usage: tcver arch")
         return
 
+    tcver = sys.argv[1]
+    arch = sys.argv[2]
+    visited = set()
+    rebuilded = set()
+
     for pkg in os.listdir("pkgs"):
-        depends(pkg, visited)
+        rebuilds(pkg, visited, rebuilded, tcver, arch)
+    return
 
 
 if __name__ == "__main__":
